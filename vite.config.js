1b4dfad0b7531dd64acd5375f0bafad70f0c6b1a
arsistemadpw13 <arsistemadpw@gmail.com>
2025-06-17 14:06:15 -0400

    commit inicial

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  //base: '/tu-proyecto-react/', // <-- nombre del repositorio
+  base: "https://profWilliamArte.github.io/themovie2025",
+});
